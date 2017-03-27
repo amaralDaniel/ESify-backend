e8ea5ce788150bf104f9d33469b7079fef766e41
amaralDaniel <amaral94daniel@gmail.com>
2017-03-27 22:34:35 +0100

    fix login bug

--- beans/AuthBean.py
+++ beans/AuthBean.py
@@ -1,4 +1,6 @@
 class AuthBean(object):
+    from passlib.hash import bcrypt
+
     def __init__(self):
         pass
 
@@ -8,9 +10,39 @@ class AuthBean(object):
         from hashlib import sha512
 
         try:
-            user = User(email, sha512(password).hexdigest(), username)
+            user = User(email,  sha512(password).hexdigest(), username)
             db.session.add(user)
             db.session.commit()
+            return True
+        except Exception as e:
+            print e
+            return False
+
+
+    def login(self, email, pwd):
+        from models import User
+        from esify import db, session
+        import uuid
+        from hashlib import sha512
+
+        try:
+            print pwd
+            pwd = sha512(pwd).hexdigest()
+            print pwd
+            u = User.query.filter_by(email=email,password=pwd).first()
+            # print u.username
+            print u.password
+            # print u.email
+            if u is None:
+                print "false"
+                return False
+
+            u.session_token = uuid.uuid4().hex
+            db.session.commit()
+
+            session["email"] = u.email
+            session["X-Auth-Token"] = u.session_token
+
             return True
         except Exception as e:
             print e
