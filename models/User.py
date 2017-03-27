ea3b6da3ce3fb13224a5f0f6cf5fa9fba9073502
amaralDaniel <amaral94daniel@gmail.com>
2017-03-27 22:34:52 +0100

    changed models

--- models/User.py
+++ models/User.py
@@ -1,10 +1,11 @@
 from esify import db
+from passlib.hash import bcrypt
 
 class User(db.Model):
     __tablename__ = 'Users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(300), nullable=False)
     username = db.Column(db.String(120), unique=True)
     session_token = db.Column(db.String(256), unique=True)
     playlists = db.relationship('Playlist',backref='user',lazy="dynamic")
@@ -12,9 +13,14 @@ class User(db.Model):
     def __init__(self, email, password, username):
         self.email = email
         self.password = password
+        # self.password = password
         self.username = username
         import uuid
         self.session_token = uuid.uuid4().hex
+
+    # def verify_password(self, password):
+    #     pwhash = bcrypt.hashpw(password, self.password)
+    #     return self.password == pwhash
 
     def __repr__(self):
         return '<username %r, >' % self.username
