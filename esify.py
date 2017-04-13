969ff574b1220817d6b456cfabcfcf8e96372db4
amaralDaniel <amaral94daniel@gmail.com>
2017-04-13 22:51:53 +0100

    esify with settings, blueprint

--- esify.py
+++ esify.py
@@ -1,25 +1,45 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_sqlalchemy import SQLAlchemy
-from flask import session
+import settings
+# from rest_api_demo.api.blog.endpoints.posts import ns as blog_posts_namespace
+# from rest_api_demo.api.blog.endpoints.categories import ns as blog_categories_namespace
+from api.restplus import api
+from models import db
 
 app = Flask(__name__)
-from routes import *
 
 SONGS_FOLDER = '/songs'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:root@localhost:3306/esify'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db = SQLAlchemy(app)
+def configure_app(flask_app):
+    flask_app.config['SERVER_NAME'] = settings.FLASK_SERVER_NAME
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = settings.SQLALCHEMY_DATABASE_URI
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    db = SQLAlchemy(flask_app)
+    flask_app.config['SWAGGER_UI_DOC_EXPANSION'] = settings.RESTPLUS_SWAGGER_UI_DOC_EXPANSION
+    flask_app.config['RESTPLUS_VALIDATE'] = settings.RESTPLUS_VALIDATE
+    flask_app.config['RESTPLUS_MASK_SWAGGER'] = settings.RESTPLUS_MASK_SWAGGER
+    flask_app.config['ERROR_404_HELP'] = settings.RESTPLUS_ERROR_404_HELP
 
-app.config['SONGS_UPLOADED'] = SONGS_FOLDER
-# set the secret key.  keep this really secret:
-app.secret_key = "\xe4IX\x1a\xf4\x9b\x80'K\xb1\xa1L\xba\xcd\xe5\xb0Ts\xfal\xe3NB\xa1"
+    flask_app.config['SONGS_UPLOADED'] = SONGS_FOLDER
+    # set the secret key.  keep this really secret:
+    flask_app.secret_key = "\xe4IX\x1a\xf4\x9b\x80'K\xb1\xa1L\xba\xcd\xe5\xb0Ts\xfal\xe3NB\xa1"
 
-if __name__ == '__main__':
-    try:
-        from models import *
-        db.create_all()
-        db.session.commit()
-    except Exception as e:
-        print e
-    app.run()
+def initialize_app(flask_app):
+    configure_app(flask_app)
+
+    blueprint = Blueprint('api', __name__, url_prefix='/api')
+    api.init_app(blueprint)
+    # api.add_namespace(blog_posts_namespace)
+    # api.add_namespace(blog_categories_namespace)
+    flask_app.register_blueprint(blueprint)
+
+    db.init_app(flask_app)
+
+def main():
+    initialize_app(app)
+    print ">>>>> Starting development server at http://{}/api/ <<<<<".format(app.config['SERVER_NAME'])
+
+    app.run(debug=settings.FLASK_DEBUG)
+
+if __name__ == "__main__":
+    main()
