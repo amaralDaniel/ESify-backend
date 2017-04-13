c212a7dd5d8bc8a34ae9441a7acd16149b445a91
amaralDaniel <amaral94daniel@gmail.com>
2017-04-13 22:50:43 +0100

    restplus added

---
+++ api/__init__.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def reset_database():
+    from User import *
+    from Song import *
+    from Playlist import *
+    
+    db.drop_all()
+    db.create_all()
