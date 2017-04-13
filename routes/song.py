6a3255acb12270db504314e53132dcdcd08e1495
Jorge Araújo <jdcmaraujo@gmail.com>
2017-04-13 22:47:00 +0100

    add song uploading

--- routes/song.py
+++ routes/song.py
@@ -3,14 +3,14 @@ import os
 from markupsafe import Markup
 import json
 
-from flask import redirect, url_for, escape, request
+from flask import redirect, url_for, escape, request, flash, send_from_directory, render_template
 
 from esify import app, session
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '/songs'
+UPLOAD_FOLDER = './uploads/'
 ALLOWED_EXTENSIONS = set(['mp3','wav','aac'])
-
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -18,23 +18,25 @@ def allowed_file(filename):
 @app.route('/song',methods=['GET', 'POST'])
 def upload_song():
     if request.method == 'POST' :
-        if 'song_file' not in request.files:
+        if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
-        song_file = request.files['song_file']
-        if song_file.filename == '':
+        file = request.files['file']
+        if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
-        if song_file and allowed_file(song_file.filename):
-            filename = secure_filename(song_file.filename)
-            song_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('uploaded_file',filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <p><input type=file name=song_file>
-         <input type=submit value=Upload>
-    </form>
-    '''
+        if file and not (allowed_file(file.filename)):
+            filename = secure_filename(file.filename)
+            flash('Incompatible media type')
+            return render_template('song.html')
+    return render_template('song.html')
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
