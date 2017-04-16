0af4b6e08f5e92e86f7104fedab1defa263dba01
amaralDaniel <amaral94daniel@gmail.com>
2017-04-16 02:29:27 +0100

    login and logout working

--- api/routes/authentication.py
+++ api/routes/authentication.py
@@ -3,9 +3,9 @@ from markupsafe import Markup
 import json
 import logging
 
-from flask import redirect, url_for, escape, request
+from flask import request
 from flask_restplus import Resource
-from api.beans.AuthBean import register, login
+from api.beans.AuthBean import register, login, logout
 from api.restplus import api
 
 
@@ -14,15 +14,8 @@ log = logging.getLogger(__name__)
 ns = api.namespace('authentication', description='Operations related to authentication')
 
 
-
 @ns.route('/register')
 class Register(Resource):
-
-    @api.response(200, 'Test')
-    def get(self):
-        """
-        Get just for testing.
-        """
 
     @api.response(200, 'User registered')
     # TODO: serializers
@@ -35,34 +28,28 @@ class Register(Resource):
         register(data)
         return None, 200
 
-# @ns.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if request.method == 'POST':
-#         email = request.form['email']
-#         password = request.form['password']
-#         username = request.form['username']
-#
-#         from beans import AuthBean
-#
-#         bean = AuthBean()
-#
-#         if bean.login( email , password):
-#             return 'LOGGED'
-#         else:
-#             return 'NOT'
-#     else:
-#         return '''
-#             <form method="post">
-#                 <p><input type=text name=email>
-#                 <p><input type=password name=password>
-#                 <p><input type=submit value=Login>
-#             </form>
-#         '''
-#
-#
+@ns.route('/login')
+class Login(Resource):
 
-# @ns.route('/logout')
-# def logout():
-#     # remove the username from the session if it's there
-#     session.pop('username', None)
-#     return redirect(url_for('index'))
+    @api.response(200, 'User logged in')
+    def post(self):
+        """
+        Enables users to login in the platform.
+        """
+        data = request.json
+
+        login(data)
+        return None, 200
+
+
+@ns.route('/logout')
+class Logout(Resource):
+
+    @api.response(200,'Logged user ')
+    def get(self):
+        """
+        Enables users to logout from the platform.
+        """
+
+        logout()
+        return None, 200
