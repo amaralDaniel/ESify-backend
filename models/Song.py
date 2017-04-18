eab89a5c539168beb4ddbb91aae1f3589c8fd983
amaralDaniel <amaral94daniel@gmail.com>
2017-04-18 19:13:41 +0100

    removed unique from title in song

--- models/Song.py
+++ models/Song.py
@@ -4,9 +4,9 @@ from models import db
 class Song(db.Model):
     __tablename__ = 'Songs'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String(120), unique=True)
+    title = db.Column(db.String(120), nullable=False)
     artist = db.Column(db.String(120), nullable=False)
-    uploader = db.Column(db.String(500), unique=True)
+    uploader = db.Column(db.String(500), nullable=False)
 
     def __init__(self, title, artist, uploader):
         self.title = title
