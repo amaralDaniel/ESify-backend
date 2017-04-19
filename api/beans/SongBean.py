7ce6d4099675d8f2b614404301b88f074284eaef
Jorge Araújo <jdcmaraujo@gmail.com>
2017-04-19 16:35:35 +0100

    add delete song

--- api/beans/SongBean.py
+++ api/beans/SongBean.py
@@ -3,7 +3,7 @@ from os.path import join, dirname, realpath
 from models import db
 from models.Song import Song
 from flask import json
-from flask import redirect, url_for, escape, request
+from flask import redirect, url_for, escape, request, session
 from werkzeug.utils import secure_filename
 
 
@@ -38,7 +38,7 @@ def upload(request):
             filename = secure_filename(file.filename)
             file.save(os.path.join(UPLOAD_FOLDER, filename))
 
-        song = Song(title, artist, user)
+        song = Song(title, artist, user.username)
         db.session.add(song)
         db.session.commit()
         return True
@@ -52,9 +52,33 @@ def get_all_songs():
 
         data = {}
         for obj in songs_list:
-            data["title_song"+str(obj.id)] = obj.title
-            data["artist_song"+str(obj.id)] = obj.artist
+            data["title_song_id:"+str(obj.id)] = obj.title
+            data["artist_song_id:"+str(obj.id)] = obj.artist
         return  json.dumps(data)
+    except Exception as e:
+        print e
+        return False
+
+
+def verify_owner(song_id, token):
+    from api.beans.AccountBean import get_user
+    user = get_user(token)
+    song = Song.query.filter_by(id=song_id).first_or_404()
+    owner = song.uploader
+    if user.username == owner:
+        return True
+    else:
+        return False
+
+def delete_song(song_id):
+    try:
+        song = Song.query.all()
+
+        for s in song:
+            if s.id == song_id:
+                db.session.delete(s)
+                db.session.commit()
+            return True
     except Exception as e:
         print e
         return False
--- api/routes/song.py
+++ api/routes/song.py
@@ -4,8 +4,8 @@ from flask import request, redirect
 from flask import jsonify
 from flask_restplus import Resource
 
-from api.beans.AuthBean import register, login, logout
-from api.beans.SongBean import upload, get_all_songs
+from api.beans.AuthBean import register, login, logout, verify_token
+from api.beans.SongBean import upload, get_all_songs, verify_owner, delete_song
 from api.restplus import api
 from models.Song import Song
 
@@ -37,3 +37,24 @@ class Songs(Resource):
             return list_songs, 200
         else:
             return "No songs found", 200
+
+
+@ns.route('/<int:id>')
+class ManageSongs(Resource):
+    @api.response(200, 'Deleted playlist ')
+    @api.response(400, 'Bad Request')
+    @api.response(403, 'Forbidden accesss')
+    def delete(self, id):
+        """
+        Delete uploaded songs
+        """
+        from esify import session
+        if not verify_token(session["X-Auth-Token"]):
+            session.clear()
+            return None, 403
+        if not verify_owner(id, session["X-Auth-Token"]):
+            return "you're not allowed", 403
+        if delete_song(id):
+            return None, 200
+        else:
+            return None, 400
